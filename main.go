32fbd477b27754ea4ab026d40087826fa4599086
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-08-09 18:46:44 -0500

    Functions

--- main.go
+++ main.go
@@ -6,20 +6,32 @@ const helloWorld string = "Hola %s %s, bienvenido al fascinante mundo de go"
 const testConst = "Test"
 
 func main() {
-	var name string
-	name = "Pablo"
 	lastname := "<Modificar con mi apellido>"
-	var number = 100
-	var (
-		a = 1
-		b = 2
-		c = 3
-	)
+	name := getName()
 
-	fmt.Print("Ingresa tu nombre: ")
-	fmt.Scanf("%s", &name)
+	number := sum(50, 50)
+	a, b, c := getVariables()
+
 	fmt.Printf(helloWorld, name, lastname)
 
 	fmt.Println("Hola mundo")
 	fmt.Println(number, a, b, c)
+}
+
+func getName() string {
+	var name string
+	name = "Sin nombre"
+
+	fmt.Print("Ingresa tu nombre: ")
+	fmt.Scanf("%s", &name)
+
+	return name
+}
+
+func getVariables() (int, int, int) {
+	return 1, 2, 3
+}
+
+func sum(a int, b int) int {
+	return a + b
 }
