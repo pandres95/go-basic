de1d6be29812ac92592d5ea702b9c7591141abf4
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-08-09 18:26:39 -0500

    Hello world

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	fmt.Print("Ingresa tu nombre: ")
+	fmt.Scanf("%s", &name)
+	fmt.Printf("Hola %s, bienvenido al fascinante mundo de go", name)
+	fmt.Println("Hola mundo")
+}
