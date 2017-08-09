bf5124de4852e97055748426e398265d45b797f1
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-08-09 18:40:22 -0500

    First variables

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import "fmt"
 
+const helloWorld string = "Hola %s %s, bienvenido al fascinante mundo de go"
+const testConst = "Test"
+
 func main() {
 	var name string
+	name = "Pablo"
+	lastname := "<Modificar con mi apellido>"
+	var number = 100
+	var (
+		a = 1
+		b = 2
+		c = 3
+	)
+
 	fmt.Print("Ingresa tu nombre: ")
 	fmt.Scanf("%s", &name)
-	fmt.Printf("Hola %s, bienvenido al fascinante mundo de go", name)
+	fmt.Printf(helloWorld, name, lastname)
+
 	fmt.Println("Hola mundo")
+	fmt.Println(number, a, b, c)
 }
